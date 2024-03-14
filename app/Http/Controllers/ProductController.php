88ec8e282a2aecccd709119eab1e78c3158e4df3
Juan <franciscodiaz.jg@gmail.com>
2024-03-14 12:52:32 -0600

    HomeWork 5

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,5 +7,19 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    
+    public function index()
+    {
+        return view('products.view');
+    }
+
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    public function store(Request $request)
+    {
+        // Logic to store the product
+    }
 }
